8ffb4d4f0be09adb5d83b8220fb6063fd007101d
Joshua Henderson <joshua.henderson@microchip.com>
2019-06-18 15:59:23 -0700

    pull from egt project
    Signed-off-by: Joshua Henderson <joshua.henderson@microchip.com>

---
+++ autogen.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+
+#
+# Copyright (C) 2018 Microchip Technology Inc.  All rights reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+autoreconf --force --verbose --install || exit 1
